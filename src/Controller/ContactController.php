e48b700892167c877e9066a108a9c64c03665531
Lukas Keževičius <l.kezevicius@gmail.com>
2019-09-24 18:10:48 +0300

    contact form basics

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,24 +1,49 @@
 <?php
+
 namespace App\Controller;
 
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use App\Form\ContactForm;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function show(Request $request)
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
-        $form = $this->createForm(ContactForm::class);
+        $form = $this->createForm(ContactFormType::class);
+
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $contactFormData = $form->getData();
+
+            // if ()
+            // {
+            //     $this->addFlash('danger', 'Error');
+            // }
+
+            $message = (new \Swift_Message('Contact form: from '. $contactFormData['name']. ' subject: '.$contactFormData['subject']))
+                ->setFrom($contactFormData['email'])
+                ->setTo('l.kezevicius@gmail.com')
+                ->setBody(
+                    $contactFormData['message'],
+                    'text/plain'
+                )
+            ;
+
+            $mailer->send($message);
+            $this->addFlash('success', 'Email sent!');
+            $this->redirectToRoute('contact');
+        }
+
         return $this->render('contact.html.twig', [
-            'contact' => $form->createView(),
+            'contact_form' => $form->createView(),
         ]);
     }
 }
